509eb9773291199ebc60d70a37e31cb14a7bb296
shabura-dmitry <103600312+shabura-dmitry@users.noreply.github.com>
2023-05-11 18:56:39 -0500

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,5 +1,14 @@
+import json
+from account import open_account
 def create_official():
-    pass
+    fname = input("First name:")
+    lname = input("Last name:")
+    pnum = input("Phone number:")
+    address = input("Address:")
+    #transaction_records = [] # unsure how to implement
+    status = 1 # for closed, 1 for open
+    account_type = 0 # 0 for customer, 1 for official, 2 for admin - probably shouldn't 
+    open_account(fname, lname, pnum, address, account_type)
 
 def get_username(fname, lname):
     pass
