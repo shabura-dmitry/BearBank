e7a7237cb86c15abbca6591480f67225f4aa73ce
shabura-dmitry <103600312+shabura-dmitry@users.noreply.github.com>
2023-05-12 18:37:01 -0500

    account

--- account.py
+++ account.py
@@ -1,17 +1,15 @@
-import json
 from datetime import date
-
-def open_account(fname, lname, pnum, address, account_type, balance,username, password):
-    
-
+from fileloading import load_file
+from fileloading import save_file
+def open_account(fname, lname, pnum, address, account_type, balance, username, password):
+    data = load_file()
     open_date = str(date.today())
     close_date = -1 #dont change until closed
-    balance = 0 
-    account_num = 0 # increment for every account made
+    account_num = len(data)+1
     status = 1 # 0 for closed, 1 for open
     account = {'fname': fname, 'lname': lname, 'pnum': pnum, 'address': address, 'open_date': open_date, 
                'close_date': close_date, 'balance': balance,'account_num': account_num,
                 'status': status, 'account_type': account_type, 'username': username, 'password': password}
+    data[account_num] = account
     
-   
-    return account
+    save_file(data)
