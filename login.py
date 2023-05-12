704936c2bdfbe0960ade2a72b01825bbbef2617f
shabura-dmitry <103600312+shabura-dmitry@users.noreply.github.com>
2023-05-12 17:57:05 -0500

    create_menu

--- login.py
+++ login.py
@@ -6,8 +6,19 @@ filename = "C:/Users/ds227s/Documents/GitHub/BearBank/user_database.json"
 def login(username, password):
     data = load_file()
     for account in range(len(data)):
-        print(len(data))
+        if data['username'] == username and data['password'] == password:
+            create_menu(data['account_type'])
     else:
         print("invalid username or password")
 
 login(username, password)
+
+def create_menu(account_type):
+    if account_type == 0:
+        pass
+    elif account_type == 1:
+        pass
+    elif account_type ==2:
+        pass
+    else:
+        print("error")
