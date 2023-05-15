a08e6b52825ec1d335624b02338b5c18bdb424c0
shabura-dmitry <103600312+shabura-dmitry@users.noreply.github.com>
2023-05-15 12:35:50 -0700

    admin functions

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 import json
 from account import open_account
 from fileloading import *
+from datetime import date
 
 data=load_file()
 
@@ -17,26 +18,35 @@ def open_official(logged_in_account):
     username = input("Username: ")
     password = input ("Password: ")
     open_account(fname, lname, pnum, address, account_type, official_id, official_fname, official_lname, balance, username, password)
-    print("Account created")
+    print("Account created\n")
 
-# return the username of a customer account given the first name, last name, and date of borth
+# return the username of a customer account given the first name, last name
 def get_username():
     fname = input("First name:")
     lname = input("Last name:")
-    dob = input("Date of birth: (YYYY-MM-DD)")
     for i in data:
-        if data[i]['fname'] == fname and data[i]['lname'] == lname and data[i]['dob'] == dob:
-            print(data[i]['username'])
+        if data[i]['fname'] == fname and data[i]['lname'] == lname:
+            print("Username:", data[i]['username'])
             return
-    print("Account not found")
+    print("Account not found\n")
 
 # changes the status of an official account to closed if open and vice versa.
-def change_status_official(account_num):
-    if data[str(account_num)]['status'] == 0:
-        data[str(account_num)]['status'] = 1
-        print("Account opened")
-    elif data[str(account_num)]['status'] == 1:
-        data[str(account_num)]['status'] = 0
-        print("Account closed")
-    save_file(data)
+def change_status_official():
+    account_num = input("Official account number:")
+    #check if account type is official
+    if data[str(account_num)]['account_type'] == 1:
+        if data[str(account_num)]['status'] == 0:
+            data[str(account_num)]['status'] = 1
+            print("Account opened")
+        elif data[str(account_num)]['status'] == 1:
+            data[str(account_num)]['status'] = 0
+            data[str(account_num)]['close_date'] = str(date.today())
+            print("Account closed")
+        save_file(data)
 
+def change_pass_as_admin():
+    account_num = input("Account number:")
+    new_pass = input("Please enter a new password:")
+    data[str(account_num)]['password'] = new_pass
+    print("Password changed\n")
+    save_file(data)
--- menu.py
+++ menu.py
@@ -11,7 +11,7 @@ def create_menu(logged_in_account):
      # if account type is 0, then it is a customer. if account type is 1, then it is an official. if account type is 2, then it is an admin
     account_type = logged_in_account['account_type']
     account_num = logged_in_account['account_num']
-    print("Welcome to BearBank!\n")
+    print("---Welcome to BearBank!---\n")
     # customer menu options: change login password, view account, view transaction history
     if account_type == 0:
         command = input("Please choose an option below:\na: change login password\nb: view account\nc:view transaction history\n")
@@ -44,13 +44,13 @@ def create_menu(logged_in_account):
     elif account_type == 2:
         command = input("Please choose an option below:\na:Open official account\nb:Change official account status\nc:Retrieve login id\nd:Change password\nb")
         if command == 'a':
-            open_official()
+            open_official(logged_in_account)
         elif command == 'b':
-            change_status_official(account_num)
+            change_status_official()
         elif command == 'c':
             get_username()
         elif command == 'd':
-            change_pass(account_num)
+            change_pass_as_admin()
         else:
             print ("error")
         create_menu(logged_in_account)
